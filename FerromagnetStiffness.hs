16c2cce219ddab51223187bf07f4b13ca2653c76
Kirill <pshcyrill@mail.ru>
2020-11-09 19:53:50 +0300

    add theta_0

--- FerromagnetStiffness.hs
+++ FerromagnetStiffness.hs
@@ -2,6 +2,12 @@ module FerromagnetStiffness where
 
 import PhysicalConsts
 
--- theta_0 (Stiffness [meVA^2]) 
+-- (theta_0 stiffness [meVA^2]) [rad]
 theta_0 :: Double->Double
-theta_0 Stiffness = h_plank*h_plank/e_electron/
+theta_0 stiffness = (h_plank/2/pi)**2/2/m_neutron/(toJoul stiffness)/1e-20  -- 1e-20 - Angstrom^2
+
+
+-- convert Joul to meV
+toMev x = 1000*x/e_electron
+-- convert meV to Joul
+toJoul x = x*e_electron/1000
