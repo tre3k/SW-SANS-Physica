7325abb91b1b199ea1a883ab519dd137eb21a1f0
Kirill <pshcyrill@mail.ru>
2020-11-09 21:16:14 +0300

    add theta_C and theta_0

--- FerromagnetStiffness.hs
+++ FerromagnetStiffness.hs
@@ -4,10 +4,36 @@ import PhysicalConsts
 
 -- (theta_0 stiffness [meVA^2]) [rad]
 theta_0 :: Double->Double
-theta_0 stiffness = (h_plank/2/pi)**2/2/m_neutron/(toJoul stiffness)/1e-20  -- 1e-20 - Angstrom^2
+theta_0 stiffness = (h_plank/2/pi)**2/2/m_neutron/(_meVToJoul stiffness)/1e-20  -- 1e-20 - Angstrom^2
+
+-- (theta_C2 stiffnedd [meVA^2] filed [T] wavelenght [Angstrom]) [rad^2]   // theta_C^2 without gap 
+theta_C2 :: Double->Double->Double->Double
+theta_C2 stiffness field lambda =
+  (theta_0 stiffness)**2 - (theta_0 stiffness)*(_TeslaTomeV field)/(neutronEnergy'meV lambda)
+theta_C stiffness field lambda = sqrt (theta_C2 stiffness field lambda)
+
+-- (theta_C2'gap stiffnedd [meVA^2] filed [T] gap [meV] wavelenght [Angstrom]) [rad^2]   // theta_C^2 with gap 
+theta_C2'gap :: Double->Double->Double->Double->Double
+theta_C2'gap stiffness field gap lambda =
+  theta_C2 stiffness (field+(_meVToTesla gap)) lambda
+theta_C'gap stiffness field gap lambda = sqrt (theta_C2'gap stiffness field gap lambda)
+
+
+-- Incedent neutron energy form wavelenght
+-- (neutronEnegrhy lambda [Angstrom]) [Joul]
+neutronEnergy'Joul lambda = h_plank**2/lambda**2/2/m_neutron/1e-20
+neutronEnergy'meV lambda = _JoulTomev (neutronEnergy'Joul lambda)   -- in meV
+neutronEnergy'K lambda = (neutronEnergy'Joul lambda)/k_boltzman  -- in Kelvin
 
 
 -- convert Joul to meV
-toMev x = 1000*x/e_electron
--- convert meV to Joul
-toJoul x = x*e_electron/1000
+_JoulTomev x = 1000*x/e_electron
+_meVToJoul x = x/_JoulTomev 1
+
+-- convert Tesla magnet field to Joul
+_TeslaToJoul x = g_factor*u_bor*x
+_JoulToTesla x = x/_TeslaToJoul 1
+
+-- convert Tesla to meV
+_TeslaTomeV x = (_JoulTomev (_TeslaToJoul x))
+_meVToTesla x = x/_TeslaTomeV 1
--- PhysicalConsts.hs
+++ PhysicalConsts.hs
@@ -5,3 +5,6 @@ k_boltzman = 1.380649e-23                  -- Joul*K^-1
 c_light = 299792458.0                      -- m/s
 e_electron = 1.602176634e-19               -- Kl
 m_neutron = 1.67492749804959595e-27        -- kg
+
+g_factor = 2.0023193043625635
+u_bor = 927.401007832828e-26               -- Joul/T
