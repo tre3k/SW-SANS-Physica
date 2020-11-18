00d176a3e29c67b79f9cd393bd3a946227cc2014
Kirill <pshcyrill@mail.ru>
2020-11-18 20:30:59 +0300

    chage Main.hs

--- Main.hs
+++ Main.hs
@@ -1,7 +1,25 @@
 module Main where
 
 import FerromagnetStiffness
+import System.Environment
+
+--data Theta = Theta {theta_x :: Double, theta_y :: Double}
+data Radius = Circle Double | Square Double
+
+space :: Radius->Double
+space (Circle x) = pi*x**2
+space (Square x) = (2*x)**2
+
+parserArgs args len = 
+  if (null args)
+  then -1
+  else calculation (args!!0)
+
+
+calculation lambda = read lambda :: Double
 
 main = do
+  args <- getArgs
   putStrLn "This is Main module"
-  putStrLn (show 123.2)
+  (putStrLn . show) (parserArgs args (length args))
+  
